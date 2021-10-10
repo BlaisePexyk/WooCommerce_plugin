96ded39b1839e48d9497cd2f827a170d11186be6
BlaisePexyk <41127905+BlaisePexyk@users.noreply.github.com>
2021-10-10 20:51:45 +0200

    Create product-email.php

---
+++ product-email.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name:
+* Plugin URI: 
+* Author: Blaise Pexyk
+* Version: 0.0.1
+* Text Domain: 
+* Opis: Wysyła automatycznie powiadomienia o potrzebie wykonania zamówienia. W momencie gdy zamówienie jesy "W trakcie realziacji". Dodaje nową opcję przypisania do produktu.
+*
